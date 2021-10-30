eaf080891a69392e3957b5edecd640b098b6815f
jjjjonathan <hornathanjon@gmail.com>
2021-10-29 21:01:50 -0700

    Add CSS module for App container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import styles from './App.module.css';
+
 const App = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <header>
         <h1 id="app-title">All Done</h1>
       </header>
