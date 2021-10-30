bc823fbcccd1ba235b9b46e821efd97660174343
jjjjonathan <hornathanjon@gmail.com>
2021-10-29 21:19:40 -0700

    Fix containers and colors

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import styles from './App.module.css';
 
 const App = () => {
   return (
-    <div className={styles.container}>
-      <header>
+    <div className={`${styles.container} container mx-auto`}>
+      <header className="text-center">
         <h1 id="app-title">All Done</h1>
       </header>
     </div>
