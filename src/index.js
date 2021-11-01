1cd4bc05cfc5bc9c5ea82e1b3a437394d146ca5e
jjjjonathan <hornathanjon@gmail.com>
2021-11-01 14:49:34 -0700

    Add react-dnd to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <DndProvider backend={HTML5Backend}>
     <App />
-  </React.StrictMode>,
+  </DndProvider>,
   document.getElementById('root'),
 );
