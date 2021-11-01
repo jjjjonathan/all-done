7a1dccca4e857c54e484758b49f003ac3e6ba8bf
jjjjonathan <hornathanjon@gmail.com>
2021-11-01 16:42:31 -0700

    Add Draggable context to Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,31 +1,41 @@
 import { useState } from 'react';
+import { Draggable } from 'react-beautiful-dnd';
 import { EditText } from 'react-edit-text';
 import 'react-edit-text/dist/index.css';
 
-const Todo = () => {
+const Todo = ({ id, index }) => {
   const [checked, setChecked] = useState(false);
   const [text, setText] = useState('Walk the dog');
 
   return (
-    <div className="text-2xl flex items-center">
-      <div className="pr-3">
-        <i
-          id="todo-checkbox"
-          className={`bi ${checked ? 'bi-check-square-fill' : 'bi-square'}`}
-          onClick={() => setChecked(!checked)}
-        ></i>
-      </div>
-      <div className="w-full">
-        <EditText
-          id="todo-text"
-          value={text}
-          onChange={(changedValue) => setText(changedValue)}
-        />
-      </div>
-      <div>
-        <i className="bi bi-grip-vertical"></i>
-      </div>
-    </div>
+    <Draggable key={id} draggableId={id} index={index}>
+      {(provided) => (
+        <div
+          className="text-2xl flex items-center"
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <div className="pr-3">
+            <i
+              id="todo-checkbox"
+              className={`bi ${checked ? 'bi-check-square-fill' : 'bi-square'}`}
+              onClick={() => setChecked(!checked)}
+            ></i>
+          </div>
+          <div className="w-full">
+            <EditText
+              id="todo-text"
+              value={text}
+              onChange={(changedValue) => setText(changedValue)}
+            />
+          </div>
+          <div>
+            <i className="bi bi-grip-vertical"></i>
+          </div>
+        </div>
+      )}
+    </Draggable>
   );
 };
 
