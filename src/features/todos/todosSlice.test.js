a86dd0d3c3217912e3183396c10dee5cb1ac6721
jjjjonathan <hornathanjon@gmail.com>
2021-11-01 21:54:41 -0700

    Refactor todos tests to include initialState

--- src/features/todos/todosSlice.test.js
+++ src/features/todos/todosSlice.test.js
@@ -1,28 +1,26 @@
 import reducer, { toggleChecked, editText, add } from './todosSlice';
 
-describe('Todos slice of state', () => {
-  test('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual([
-      {
-        text: 'Walk the dog',
-        checked: false,
-        id: '7f2j48',
-      },
-      {
-        text: 'Wash dishes',
-        checked: false,
-        id: '482jfs',
-      },
-      {
-        text: 'Call Debra',
-        checked: false,
-        id: '84jd2n',
-      },
-    ]);
-  });
+const initialState = [
+  {
+    text: 'Walk the dog',
+    checked: false,
+    id: '7f2j48',
+  },
+  {
+    text: 'Wash dishes',
+    checked: false,
+    id: '482jfs',
+  },
+  {
+    text: 'Call Debra',
+    checked: false,
+    id: '84jd2n',
+  },
+];
 
+describe('Todos slice of state', () => {
   test('should handle check toggle', () => {
-    expect(reducer(undefined, toggleChecked('482jfs'))).toEqual([
+    expect(reducer(initialState, toggleChecked('482jfs'))).toEqual([
       {
         text: 'Walk the dog',
         checked: false,
@@ -44,7 +42,7 @@ describe('Todos slice of state', () => {
   test('should handle text edit', () => {
     expect(
       reducer(
-        undefined,
+        initialState,
         editText({
           text: 'Call Deborah',
           id: '84jd2n',
