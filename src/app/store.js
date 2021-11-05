e0aa46d5c990a1a275797d51841f65e6ec0bab33
jjjjonathan <hornathanjon@gmail.com>
2021-11-04 21:01:25 -0700

    Add redux local storage persistence

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todosReducer from '../reducers/todosSlice';
+import { compose } from 'redux';
+import persistState from 'redux-localstorage';
+
+const options = {
+  key: 'allDone',
+};
+
+const enhancer = compose(persistState(undefined, options));
 
 export default configureStore({
   reducer: {
     todos: todosReducer,
   },
+  enhancers: [enhancer],
 });
