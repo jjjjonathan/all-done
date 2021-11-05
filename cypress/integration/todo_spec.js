57ac0c021c37969be043a0786205154827d6d8c9
jjjjonathan <hornathanjon@gmail.com>
2021-11-04 22:00:36 -0700

    Add some integration tests

--- cypress/integration/todo_spec.js
+++ cypress/integration/todo_spec.js
@@ -1,5 +1,24 @@
-describe('The Home Page', () => {
+describe('All Done', () => {
   it('successfully loads', () => {
     cy.visit('/');
+  });
+});
+
+describe('Todo', () => {
+  it('becomes checked when clicked', () => {
+    cy.get('.bi-square').first().click();
+    expect(cy.get('.bi-check-square-fill'));
+  });
+
+  it('should be editable', () => {
+    cy.contains('Walk the dog').click();
+    cy.get('input').type(' and the cat{enter}');
+    expect(cy.contains('Walk the dog and the cat'));
+  });
+
+  it('should be able to be created', () => {
+    cy.contains('New Todo').click();
+    cy.get('input').type('Go to the movies{enter}');
+    expect(cy.contains('Go to the movies'));
   });
 });
